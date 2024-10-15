85ec7b8d03628683cbc579aa2c9ed3a4d8281369
Josh Endemann <jahend44@gmail.com>
2024-10-14 19:49:30 -0500

    Update app.js
    Read our data by creating a tours object and having it read the files in our tours-simple.json file.
    
    Then format response .

--- app.js
+++ app.js
@@ -2,19 +2,31 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+)
 
-app.get('/', (req, res) => {
-    res
-    .status(200)
-    .json({ 
-        message: 'Hello from the server side', 
-        app: 'Natours' 
-    });
-});
+// app.get('/', (req, res) => {
+//     res
+//     .status(200)
+//     .json({ 
+//         message: 'Hello from the server side', 
+//         app: 'Natours' 
+//     });
+// });
 
-app.post('/', (req, res) => {
-    res.send('You can post to this endpoint...');
-});
+// app.post('/', (req, res) => {
+//     res.send('You can post to this endpoint...');
+// });
+
+app.get('api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tours
+        }
+    })
+})
 
 const port = 3000;
 app.listen(3000, () => {
