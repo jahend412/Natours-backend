a767c5fed4e894de65e2c32351d5c22fcef19b30
Josh Endemann <jahend44@gmail.com>
2024-10-15 20:38:07 -0500

    Update app.js
    Added a POST route to create new tours in the API. First, I added a middleware to parse incoming JSON data using express.json(). In the POST request handler, I generated a new tour id, merged it with the incoming data (req.body), and pushed the new tour to the existing tours array. The updated tours data is then written back to the tours-simple.json file using fs.writeFile(), and a successful response is sent to the client.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const fs = require('fs');
+
 const app = express();
+
+// Middleware
+app.use(express.json());
 
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -19,17 +23,39 @@ const tours = JSON.parse(
 //     res.send('You can post to this endpoint...');
 // });
 
-app.get('api/v1/tours', (req, res) => {
+app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         status: 'success',
         results: tours.length,
         data: {
             tours
         }
-    })
-})
+    });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+    
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        err => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    tour: newTour
+                }
+            });
+        }
+    );
+});
+
 
 const port = 3000;
-app.listen(3000, () => {
-    console.log(`App running on ${port}`);
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
 });
