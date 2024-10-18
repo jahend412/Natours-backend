118a8358cc5b9e63c053766eb8093177085651b8
Josh Endemann <jahend44@gmail.com>
2024-10-17 21:02:56 -0500

    Created new tour and user controllers
    Moved the controllers to tourController folder and userController folder.

---
+++ controllers/userController.js
@@ -0,0 +1,39 @@
+// Get all users
+exports.getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    });
+}
+
+// Get one user
+exports.getUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    });
+}
+
+// Create user
+exports.createUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    });
+}
+
+// Update user
+exports.updateUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    });
+}
+
+// Delete user
+exports.deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    });
+}
