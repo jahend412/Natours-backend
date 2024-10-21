4145f4813b12ecc66a76b08c361576661927dcf2
Josh Endemann <jahend44@gmail.com>
2024-10-21 18:48:32 -0500

    Add checkID milleware to validate tour ID's
    Implemented checkID middleware to verify if the tour ID in the route is valid.
    
    Bound checkID to all routes with 'id' parameter using router.param().
    
    Sends a 404 error if an invalid ID is provided otherwise proceeds to the next middleware.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,17 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 )
 
+exports.checkID = (req, res, next, val) => {
+    console.log(`Tour id is: ${val}`);
+if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+        status: 'fail',
+        message: 'Invalid ID'
+    });
+}
+next();
+}
+
 // Get all tours
 exports.getAllTours =  (req, res) => {
     res.status(200).json({
@@ -62,12 +73,6 @@ exports.createTour = (req, res) => {
 
 // Update tour
 exports.updateTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
     res.status(200).json({
         status: 'success',
         data: {
@@ -78,15 +83,9 @@ exports.updateTour = (req, res) => {
 
 // Delete tour
 exports.deleteTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-
     res.status(204).json({
         status: 'success',
         data: null 
     });
 }
+
