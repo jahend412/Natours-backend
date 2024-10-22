d1091908274c105735a6fb547ad114acad72bf95
Josh Endemann <jahend44@gmail.com>
2024-10-21 19:37:17 -0500

    add checkBody middleware
    Added checkBody middleware to validate request body for tours.
    
    - Implemented checkBody middleware to ensure 'name' and 'price' fields are present.
    
    - Added checkBody to POST route in tourRoutes,js to validate incoming tour data before create new tour.
    
    - Returns a 400 error if either 'name' or 'priice' is missing from the request.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -15,6 +15,16 @@ if (req.params.id * 1 > tours.length) {
 next();
 }
 
+exports.checkBody = (req, res, next) => {
+    if (!req.body.name || !req.body.price) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Missing name or price'
+      });
+    }
+    next();
+  };
+
 // Get all tours
 exports.getAllTours =  (req, res) => {
     res.status(200).json({
