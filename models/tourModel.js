c5031ca3b0afb3b96e9b08aeabca4c267dc80eee
Josh Endemann <jahend44@gmail.com>
2024-11-06 20:44:25 -0600

    Update tourModel.js
    WE added more data paths to our tourModel file

--- models/tourModel.js
+++ models/tourModel.js
@@ -13,7 +13,28 @@ const tourSchema = new mongoose.Schema({
     price: {
         type: Number,
         required: [true, 'A tour must have a price']
-    }
+    },
+     priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a summary']
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image']
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        select: false
+    },
+    startDates: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
