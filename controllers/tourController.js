25548b6f6342472bfee70635c98c6b33ee883339
Josh Endemann <jahend44@gmail.com>
2024-11-11 19:16:14 -0600

    Add Alias middleware
    Add aliasTopTours middleware to set query parameters for top 5 cheap tours and update tourRoutes to use alias middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require('../models/tourModel');
 
+// Middleware
+exports.aliasTopTours = (req, res, next) => {
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price';
+    req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+    next();
+}
 
 // Get all tours
 exports.getAllTours = async (req, res) => {
@@ -43,7 +50,7 @@ exports.getAllTours = async (req, res) => {
     // Execute query
     const tours = await query;
     
-    
+
     // Send response
     res.status(200).json({
         status: 'success',
