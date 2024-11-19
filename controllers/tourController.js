658767bdea1cdc8b86bc4a8ada8cea5242482768
Josh Endemann <jahend44@gmail.com>
2024-11-18 18:30:48 -0600

    Update tourController.js
    Add aggregation pipeline to calculate tour stats with filtering, grouping, and sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -101,9 +101,7 @@ exports.updateTour = async (req, res) => {
             message: err
         });
     }
-
-
-        
+ 
    
 }
 
@@ -122,4 +120,44 @@ exports.deleteTour = async (req, res) => {
         message: err
     });
 }
+}
+
+// Get tour stats
+exports.getTourStats = async (req, res) => {
+    try {
+        const stats = await Tour.aggregate([
+            {
+                $match: { ratingsAverage: { $gte: 4.5 } }
+            },
+            {
+                $group: {
+                    _id: { $toUpper: '$difficulty' },
+                    numTours: { $sum: 1 },
+                    numRatings: { $sum: '$ratingsQuantity' },
+                    avgRating: { $avg: '$ratingsAverage' },
+                    avgPrice: { $avg: '$price' },
+                    minPrice: { $min: '$price' },
+                    maxPrice: { $max: '$price' }
+                }
+            },
+            {
+                $sort: { avgPrice: 1 }
+            },
+            // {
+            //     $match: { _id: { $ne: 'EASY' } }
+            // }
+        ]);
+    
+        res.status(200).json({
+            status: 'success',
+            data: {
+                stats
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 }
