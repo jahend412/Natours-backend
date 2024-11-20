78c117a5516927c4220a5d2e68c4f9e9babfc151
Josh Endemann <jahend44@gmail.com>
2024-11-19 18:27:11 -0600

    Update tourModel.js
    feat: Add query middleware for filtering secret tours and logging query execution time
    
    Implemented pre and post query middleware on the `tourSchema`. The pre-middleware filters out secret tours, and the post-middleware logs the time taken for query execution along with the returned documents.

--- models/tourModel.js
+++ models/tourModel.js
@@ -51,6 +51,22 @@ tourSchema.pre('save', function(next) {
     next();
 });
 
+// QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function(next) {  // This is a regular expression that means that it will run for all the methods that start with 'find'  
+    this.find({ secretTour: { $ne: true } });
+
+    this.start = Date.now();
+    next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+    console.log(`Query took ${Date.now() - this.start} milliseconds`);
+    console.log(docs);
+    next();
+});
+
+
+
 
 const Tour = mongoose.model('Tour', tourSchema);
 
