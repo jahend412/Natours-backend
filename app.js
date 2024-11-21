955a31e94cc87222cb775ddf83d9a583fa93c4be
Josh Endemann <jahend44@gmail.com>
2024-11-21 10:37:15 -0600

    Update app.js
    Added Error Handling to app.js
    
    -This will send a 404 error if there is an undefined route present.

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+ //  Error handling   * This is a catch-all route handler
+ app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server!`
+    });
+});
+
+
 // 1. Middleware
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -15,9 +24,15 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+});
+
 // 2. Routes
 
     app.use('/api/v1/tours', tourRouter);
     app.use('/api/v1/users', userRouter);
 
+   
 module.exports = app;
