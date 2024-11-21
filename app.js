6a5b9b477b6c042d5fabb767f17c90cba0cf04a5
Josh Endemann <jahend44@gmail.com>
2024-11-21 12:03:56 -0600

    Update app.js
    Refactor code for centralized error handling

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -27,22 +28,11 @@ app.use((req, res, next) => {
 
 //  Error handling   * This is a catch-all route handler
  app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl} on this server!`
-    });
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 //  Global error handling middleware
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
-})
+app.use(globalErrorHandler);
 
    
 module.exports = app;
