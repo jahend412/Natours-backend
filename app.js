762d7286a80424be0dfdb960c59ec794d6124f3d
Josh Endemann <jahend44@gmail.com>
2024-11-21 11:29:33 -0600

    Update app.js
    Add catch-all route and global error handling middleware
    
    - Implemented a catch-all route to handle undefined routes and return a 404 error.
    - Created global error handling middleware to standardize error responses.
    - Improved error structure by including 'status' and 'statusCode' fields.

--- app.js
+++ app.js
@@ -7,15 +7,6 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
- //  Error handling   * This is a catch-all route handler
- app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl} on this server!`
-    });
-});
-
-
 // 1. Middleware
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -33,6 +24,25 @@ app.use((req, res, next) => {
 
     app.use('/api/v1/tours', tourRouter);
     app.use('/api/v1/users', userRouter);
+
+//  Error handling   * This is a catch-all route handler
+ app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server!`
+    });
+});
+
+//  Global error handling middleware
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
+})
 
    
 module.exports = app;
