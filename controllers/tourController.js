efffb8ef4c97677462b38144ff90c3c94f5d6f20
Josh Endemann <jahend44@gmail.com>
2024-11-21 13:11:41 -0600

    Update tourController.js
    Implement catchAsync to clean up code
    
    -imported catchAsync from utils folder
    -  Added catchAsync to all route handlers and refactored code to remove the try catch block.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apifeatures');
+const catchAsync = require('../utils/catchAsync');
 
 
 // Middleware
@@ -11,8 +12,7 @@ exports.aliasTopTours = (req, res, next) => {
 }
 
 // Get all tours
-exports.getAllTours = async (req, res) => {
-   try {
+exports.getAllTours = catchAsync(async (req, res, next) => {
 
     // Execute query
     const features = new APIFeatures(Tour.find(), req.query)
@@ -22,7 +22,6 @@ exports.getAllTours = async (req, res) => {
         paginate();
 
     const tours = await features.query;
-    
 
     // Send response
     res.status(200).json({
@@ -32,17 +31,11 @@ exports.getAllTours = async (req, res) => {
             tours
         }
     });
-   } catch (err) {
-    res.status(404).json({
-        status: 'fail',
-        message: err
-    });
-   }
-}
+});
 
 // Get one tour
-exports.getTour = async (req, res) => {
-    try {
+exports.getTour = catchAsync(async (req, res, next) => {
+    
     const tour = await Tour.findById(req.params.id);
     // Tour.findOne({ _id: req.params.id })
     res.status(200).json({
@@ -51,22 +44,11 @@ exports.getTour = async (req, res) => {
             tour
         }
     });
-} catch (err) {
-    res.status(404).json({
-        status: 'fail',
-        message: err
-    });
-}
-};
+});
+
 
 // Create tour
-exports.createTour = async (req, res) => {
-    try {
-    // console.log(req.body);
-
-    // const newTour = new Tour({});
-    // newTour.save()
-    
+exports.createTour = catchAsync(async (req, res, next) => {
     const newTour = await Tour.create(req.body);  //  We call the create method right on the model
     
     res.status(201).json({
@@ -75,17 +57,10 @@ exports.createTour = async (req, res) => {
              tour: newTour
          }
     });
-    } catch (err) {
-    res.status(400).json({
-        status: 'fail',
-        message: err // Dont do this in production
-    });
-    }
-};
+});
 
 // Update tour
-exports.updateTour = async (req, res) => {
-    try {
+exports.updateTour = catchAsync(async (req, res, next) => {
         const tour = await Tour.findByIdAndUpdate (req.params.id, req.body, {
                 new: true,
                 runValidators: true
@@ -96,36 +71,21 @@ exports.updateTour = async (req, res) => {
                     tour
                 }
             })
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err
-        });
-    }
- 
-   
-}
+});
 
 // Delete tour
-exports.deleteTour = async (req, res) => {
-    try {
+exports.deleteTour = catchAsync(async (req, res, next) => {
+    
         await Tour.findByIdAndDelete(req.params.id);
     
     res.status(204).json({
         status: 'success',
         data: null 
     });
-} catch (err) {
-    res.status(404).json({
-        status: 'fail',
-        message: err
-    });
-}
-}
+});
 
 // Get tour stats
-exports.getTourStats = async (req, res) => {
-    try {
+exports.getTourStats = catchAsync(async (req, res, next) => {
         const stats = await Tour.aggregate([
             {
                 $match: { ratingsAverage: { $gte: 4.5 } }
@@ -155,17 +115,10 @@ exports.getTourStats = async (req, res) => {
                 stats
             }
         });
-    } catch (err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err
-        });
-    }
-}
+})
 
 // Get monthly plan
-exports.getMonthlyPlan = async (req, res) => {
-    try {
+exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
         const year = req.params.year * 1; // 2021
 
         const plan = await Tour.aggregate([
@@ -209,10 +162,4 @@ exports.getMonthlyPlan = async (req, res) => {
                 plan
             }
         });
-    } catch (err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err
-        });
-    }
-}
+});
