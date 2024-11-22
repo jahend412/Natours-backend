e217da589b8accfb54edb0780ab5a2f8456d76ff
Josh Endemann <jahend44@gmail.com>
2024-11-21 19:54:18 -0600

    Create userModel.js
    feat(userModel): add User schema with validation and password encryption
    
    - Defined `userSchema` with fields: name, email, photo, password, and passwordConfirm.
    - Added validations for email format, unique constraint, and minimum password length.
    - Implemented custom validation to ensure `passwordConfirm` matches `password`.
    - Included document middleware to hash passwords before saving, using bcrypt with a cost factor of 12.
    - Prevented `passwordConfirm` from being persisted in the database.
    - Exported the User model for use in authentication and user-related features.

---
+++ models/userModel.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: [true, 'Please tell us your name!'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide your email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email'], 
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: 8,
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password'],
+        validate: {
+            // This only works on SAVE!!!
+            validator: function(el) {
+                return el === this.password;
+            },
+            message: 'Passwords are not the same!',
+        }
+    }
+});
+
+
+// Document middleware: runs before .save() and .create()
+userSchema.pre('save', async function(next) {
+    // Only run this function if the password was actually modified
+    if(!this.isModified('password')) return next();
+    // Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);  // 12 is the cost parameter and a general value
+    // Delete the passwordConfirm field
+    this.passwordConfirm = undefined;  // We don't want to persist the passwordConfirm in the database
+    next();  // We need to call next() to move on to the next middleware
+})
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
