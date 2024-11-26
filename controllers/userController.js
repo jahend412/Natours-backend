9e746464ac4a66540f2b4f5bc21ba26e3ebde65f
Josh Endemann <jahend44@gmail.com>
2024-11-25 19:13:13 -0600

    Update userController.js
    Implement getAllUsers functionality to fetch and return all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,20 @@
+const User = require('../models/userModel');
+const APIFeatures = require('../utils/apifeatures');
+const catchAsync = require('../utils/catchAsync');
+
 // Get all users
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined'
+exports.getAllUsers = catchAsync(async(req, res, next) => {
+    const users = await User.find();
+
+    // Send response
+    res.status(200).json({
+        status: 'success',
+        results: users.length,
+        data: {
+            users
+        }
     });
-}
+});
 
 // Get one user
 exports.getUser = (req, res) => {
