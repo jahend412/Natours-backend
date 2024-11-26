30b2b06e8347aa33fa8a5f446921163aaddd1d59
Josh Endemann <jahend44@gmail.com>
2024-11-25 18:22:24 -0600

    Update authController.js
    Implement josnwebtoken.
    
    -  I declared my own unique 32 digit code for the JWT_SECRET in my config file and set an expiration .
    - I then put this into my req.body.
    - I also changed the req.body to the specified needed elements in the req.body for security puroposes.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,27 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const { config } = require('dotenv');
+
+config({ path: './config.env' });
+
 
 exports.signup = catchAsync(async (req, res) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
+
+    // Create a token for the user and send it in the response body 
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {  // The token is signed with the user's id and the secret key
+        expiresIn: process.env.JWT_EXPIRES_IN  // The token expires in 90 days
+    });
+
     res.status(201).json({
         status: 'success',
+        token,  
         data: {
             user: newUser
         }
