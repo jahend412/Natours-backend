c5eb2aaecae64629a42ee3c5b673f9764a9d5089
Josh Endemann <jahend44@gmail.com>
2024-11-26 19:47:36 -0600

    add protect middleware to secure routes
    -Implemented protect middleware to secure routes by verifying user authentication through JWT.
    
    Features:
    1. Checks for the presence of a valid token in the Authorization header.
    2. Verifies the token using jwt.verify and the secret key (JWT_SECRET).
    3. Ensures the user associated with the token still exists in the database.
    4. Confirms that the user has not changed their password after the token was issued.
    
    -Grants access to protected routes by attaching the authenticated user to req.user.
    
    -Enhances security for sensitive operations by requiring proper authentication.

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const { promisify } = require('util');
 
 const signToken = id => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {  // The token is signed with the user's id and the secret key
@@ -52,4 +53,43 @@ exports.login = catchAsync(async(req, res, next) => {
         token, 
     })
 
+});
+
+// Middleware to protect routes
+
+exports.protect = catchAsync(async (req, res, next) => {
+
+    // 1) Getting token and check if it's there
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')
+    ) {
+        token = req.headers.authorization.split(' ')[1];  // The token is in the second position of the array
+    }
+    console.log(token);
+    
+    if (!token) {
+        return next(new AppError('You are not logged in! Please log in to get access.', 401));
+    }
+    // 2) Verification token
+    const decoded =await promisify(jwt.verify)(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return next(new AppError('Invalid token. Please log in again!', 401));
+        }
+        console.log(decoded);
+    });
+    
+    // 3) Check if user still exists
+   const currentUser = await User.findById(decoded.id);
+   if (!currentUser) {
+        return next(new AppError('The user belonging to this token does no longer exist.', 401));
+   }
+   
+    // 4) Check if user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next(new AppError('User recently changed password! Please log in again.', 401));
+    }
+
+    // GRANT ACCESS TO PROTECTED ROUTE
+    req.user = currentUser;
+    next();
 });
