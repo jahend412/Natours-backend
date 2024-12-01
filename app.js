6562c91348dc1eb0b6c3ed0224326b7f4ef28837
Josh Endemann <jahend44@gmail.com>
2024-12-01 15:53:59 -0600

    Update app.js
    Implement helmet.
    
    Named all the global Middleware through commenting

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,21 +11,31 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1. Global Middleware
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+// Limit requests from the same IP
 const limiter = rateLimit({
     max: 100,
     windowMs: 60 * 60 * 1000,
     message: 'Too many requests from this IP.  Please try again in an hour!'
 });
 
-app.use('/api', limmiter);
+app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
