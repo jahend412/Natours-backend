17c913b6d100d782c0a44151bf991a31d22c9dc1
Josh Endemann <jahend44@gmail.com>
2024-12-01 15:40:47 -0600

    Update authController.js
    feat: add secure JWT cookie implementation for authentication
    
    - Configured cookies to include JWT with an expiration time based on environment variables.
    - Added `httpOnly` option to enhance security by preventing client-side access to the cookie.
    - Enabled `secure` flag for cookies in production to ensure transmission over HTTPS.

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,12 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id); 
+const cookieOptions = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),  // 24 hours * 60 minutes * 60 seconds * 1000 milliseconds converts days to milliseconds
+    httpOnly: true  // The cookie cannot be accessed or modified in any way by the browser
+};
+if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+res.cookie('jwt', token, cookieOptions);
 
     res.status(statusCode).json({
         status: 'success',
