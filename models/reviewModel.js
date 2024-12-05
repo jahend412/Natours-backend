f6a4a7b0543f65b8fbdba5885aaa053d2d3cf13c
Josh Endemann <jahend44@gmail.com>
2024-12-04 19:47:55 -0600

    Update reviewModel.js
    We populate the reviewSchema with tour and user.  This does add time for the query to load.  Just to keep in mind for larger scales.

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -33,6 +33,19 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'user',
+    select: 'name photo',
+  });
+  this.populate({
+    path: 'tour',
+    select: 'name',
+  });
+
+  next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = mongoose.model('Review', reviewSchema);
