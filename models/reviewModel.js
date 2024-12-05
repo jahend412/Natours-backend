299ad0f13044c00eae1997c909dcd425389c0a19
Josh Endemann <jahend44@gmail.com>
2024-12-04 20:07:09 -0600

    Update reviewModel.js
    WE populate the user and name photo to the reviewSchema.
    Take away the tour to prevent calling unnecessary data

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,15 +34,18 @@ const reviewSchema = new mongoose.Schema(
 );
 
 reviewSchema.pre(/^find/, function (next) {
+  //   this.populate({
+  //     path: 'user',
+  //     select: 'name photo',
+  //   });
+  //   this.populate({
+  //     path: 'tour',
+  //     select: 'name',
+  //   });
   this.populate({
     path: 'user',
     select: 'name photo',
   });
-  this.populate({
-    path: 'tour',
-    select: 'name',
-  });
-
   next();
 });
 
