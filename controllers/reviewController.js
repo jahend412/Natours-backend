4b894f288a9946d496b4b7333a98eb2455bac03d
Josh Endemann <jahend44@gmail.com>
2024-12-05 19:06:44 -0600

    Update reviewController.js
    - Fixed bug that was crashing my database.  Simple fix entered in the wrong location for Review and catchAsync.
    
    - If the tour and user is not provided, we set it the the tour/user Id in the request

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,5 @@
-const Review = mongoose.model('Review', reviewSchema);
-const catchAsync = require('./utils/catchAsync');
+const Review = require('./../models/reviewModel');
+const catchAsync = require('./../utils/catchAsync');
 
 // Review routes
 
@@ -18,6 +18,10 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 
 // Create review
 exports.createReview = catchAsync(async (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId; //  If tour is not provided, set it to the tour ID in the request URL
+  if (!req.body.user) req.body.user = req.user.id; //  If user is not provided, set it to the user ID in the request object
+
   const newReview = await Review.create(req.body);
 
   res.status(201).json({
