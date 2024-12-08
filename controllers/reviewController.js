34df1f35456f2587d6ee2b16c67b7444f141ec76
Josh Endemann <jahend44@gmail.com>
2024-12-08 17:15:21 -0600

    Update reviewController.js
    feat: add filtering by tourId to getAllReviews route
    
    - Updated `getAllReviews` to support filtering reviews by `tourId` from route parameters.
    - Applied dynamic filter logic to fetch reviews associated with a specific tour when `tourId` is provided.
    - Ensured functionality remains intact for fetching all reviews when no `tourId` is specified.

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -5,7 +5,10 @@ const catchAsync = require('./../utils/catchAsync');
 
 // Get all reviews
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const review = await Review.find();
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+
+  const review = await Review.find(filter);
 
   res.status(200).json({
     status: 'success',
