b7a275d02b031d25da6187d4162cead9c12da1d8
Josh Endemann <jahend44@gmail.com>
2024-12-08 17:45:00 -0600

    Update reviewRoutes.js
    Created a route to delete Reviews.   Currently not set to need authentication.

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -13,4 +13,9 @@ router
     reviewController.createReview,
   );
 
+router.route('/:id').delete(
+  // authController.protect,
+  // authController.restrictTo,
+  reviewController.deleteReview,
+);
 module.exports = router;
