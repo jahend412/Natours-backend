f978c873cb242aacd03707b221359ca7f8146acc
Josh Endemann <jahend44@gmail.com>
2024-12-08 19:14:19 -0600

    Update reviewRoutes.js

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,29 +1,30 @@
+const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
-const express = require('express');
 
 const router = express.Router({ mergeParams: true });
+
+router.use(authController.protect);
 
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
-    authController.restrictTo,
+    authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview,
   );
 
 router
   .route('/:id')
+  .get(reviewController.getReview)
   .patch(
+    authController.restrictTo('user', 'admin'),
     reviewController.updateReview,
-    // authController.protect,
-    // authController.restrictTo,
   )
   .delete(
-    // authController.protect,
-    // authController.restrictTo,
+    authController.restrictTo('user', 'admin'),
     reviewController.deleteReview,
   );
+
 module.exports = router;
