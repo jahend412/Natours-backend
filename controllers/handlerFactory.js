d23f5a7d0b851649898244b2fcdd3a9edfbd18ae
Josh Endemann <jahend44@gmail.com>
2024-12-08 18:18:21 -0600

    Update handlerFactory.js
    feat: implement reusable createOne and updateOne factory functions for CRUD operations
    
    - Added `createOne` factory function to handle the creation of documents for any Mongoose model.
      - Returns a 201 response with the created document.
      - Includes error handling to handle edge cases where the document creation fails.
    
    - Added `updateOne` factory function to handle updating documents by ID for any Mongoose model.
      - Ensures updated document is returned (`new: true`) and validated against schema rules (`runValidators: true`).
      - Includes error handling to return a 404 response if no document is found with the given ID.
    
    - Utilized `catchAsync` for centralized async error handling in both functions.

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,6 +1,25 @@
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
+// Create a document
+exports.createOne = (Model) => {
+  return catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+};
+
+// Delete a document
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
@@ -12,5 +31,25 @@ exports.deleteOne = (Model) =>
     res.status(204).json({
       status: 'success',
       data: null,
+    });
+  });
+
+// Update a document
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
     });
   });
