ae334a19dcefb5a1b4f8902ee1a1ba1f9595abac
Josh Endemann <jahend44@gmail.com>
2024-12-08 18:19:49 -0600

    feat: integrate createOne factory function for tour creation
    - Simplified tour creation by using the reusable `createOne` factory function.
    - Ensures consistent and streamlined logic for creating new tour documents.
    - Leverages centralized error handling and reusable patterns for CRUD operations.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -51,51 +51,13 @@ exports.getTour = catchAsync(async (req, res, next) => {
 });
 
 // Create tour
-exports.createTour = catchAsync(async (req, res, next) => {
-  const newTour = await Tour.create(req.body); //  We call the create method right on the model
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour: newTour,
-    },
-  });
-});
+exports.createTour = factory.createOne(Tour);
 
 // Update tour
-exports.updateTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
-
-  if (!tour) {
-    return next(new AppError('No tour found with that ID', 404));
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-});
+exports.updateTour = factory.updateOne(Tour);
 
 // Delete tour
 exports.deleteTour = factory.deleteOne(Tour);
-// // Delete tour
-// exports.deleteTour = catchAsync(async (req, res, next) => {
-//   const tour = await Tour.findByIdAndDelete(req.params.id);
-
-//   if (!tour) {
-//     return next(new AppError('No tour found with that ID', 404));
-//   }
-
-//   res.status(204).json({
-//     status: 'success',
-//     data: null,
-//   });
-// });
 
 // Get tour stats
 exports.getTourStats = catchAsync(async (req, res, next) => {
