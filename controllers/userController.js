941bee78dab9197e04b58bb31170e20aa9402385
Josh Endemann <jahend44@gmail.com>
2024-12-09 18:35:59 -0600

    Update userController.js
    Create a get Current User middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,6 +21,11 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user posts password data
   if (req.body.password || req.body.passwordConfirm) {
